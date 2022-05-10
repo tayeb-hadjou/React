9194c51dbdd7797f597dc5ba8d33ba7a91b78304
massi <massi@mx>
2022-05-10 21:54:39 +0200

    mouse click 1

--- meme_generator/src/components/form.js
+++ meme_generator/src/components/form.js
@@ -1,14 +1,17 @@
+import { func } from 'prop-types';
 import React from 'react';
+import Memes from '../data/memesData.js';
 export default function Form() {
-    const valButton='GEt a new meme image'
+    const valButton='GEt a new meme image';
+    function clickEvent(e) {
+        ReactDOM.render(<img src={Memes.data.memes[0].url} />, document.getElementById('meme'));
+    }
     return (
         <div className='form'>
-        <form>
                 <input type='text' placeholder='top text'></input>
                 <input type='text' placeholder='bottom text'></input>
-                <button className='genButton'>{valButton}</button>
+                <button className='genButton' onClick={clickEvent}>{valButton}</button>
 
-        </form>
         </div>
     )
 }
--- meme_generator/src/components/meme.js
+++ meme_generator/src/components/meme.js
@@ -2,8 +2,7 @@ import React from 'react';
 export default function Meme() {
     
     return (
-        <div className='meme'>
-            <p>the meme here</p>
+        <div id='meme'>
         </div>
     )
 }
