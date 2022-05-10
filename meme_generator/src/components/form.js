be509c31edadf3d578453fd7ae5c731d605af6ba
massi <massi@mx>
2022-05-10 19:42:50 +0200

    form

--- meme_generator/src/components/form.js
+++ meme_generator/src/components/form.js
@@ -1,8 +1,14 @@
 import React from 'react';
 export default function Form() {
+    const valButton='GEt a new meme image'
     return (
         <div className='form'>
-        <p>the form here</p>
+        <form>
+                <input type='text' placeholder='top text'></input>
+                <input type='text' placeholder='bottom text'></input>
+                <button className='genButton'>{valButton}</button>
+
+        </form>
         </div>
     )
 }
--- meme_generator/src/components/meme.js
+++ meme_generator/src/components/meme.js
@@ -1,8 +1,9 @@
 import React from 'react';
 export default function Meme() {
+    
     return (
         <div className='meme'>
-        <p>the meme here</p>
+            <p>the meme here</p>
         </div>
     )
 }
