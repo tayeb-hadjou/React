e44360afd7d6ae4f6e68a26f727ffe4568bb49f1
massi <massi@mx>
2022-05-10 23:42:49 +0200

    new organization of components to avoid flow inversion

--- meme_generator/src/components/app.js
+++ meme_generator/src/components/app.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from '../components/header.js';
-import Form from '../components/form.js';
 import Meme from '../components/meme.js';
 export default function App() {
   return (
     <div >    
     <Header/>
-    <Form/>
     <Meme/>
     </div>
   )
--- meme_generator/src/components/meme.js
+++ meme_generator/src/components/meme.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Memes from '../data/memesData.js';
 export default function Meme() {
-    
+    const [memImage,setMemeImage]=React.useState('');
+    function clickEvent(e) {
+        const meme = Memes.data.memes[Math.floor(Math.random() * Memes.data.memes.length)];
+        setMemeImage(meme);
+    }
+    const valButton='GEt a new meme image';
     return (
-        <div id='meme'>
+    <div id='meme'>
+        <div className='form'>
+            <input type='text' placeholder='top text'></input>
+            <input type='text' placeholder='bottom text'></input>
+            <button className='genButton' onClick={clickEvent}>{valButton}</button>
         </div>
+            {memImage && <img src={memImage.url} alt='meme' className='memeImage'/>}
+        
+        
+    </div>
     )
 }
